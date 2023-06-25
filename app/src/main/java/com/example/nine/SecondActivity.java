b3946d3d6d65b178b21365afcf5bf4dfd8bb2c39
Margaret <maenunezt@gmail.com>
2023-06-24 20:59:26 -0400

    ejercicio 9 terminado

--- app/src/main/java/com/example/nine/SecondActivity.java
+++ app/src/main/java/com/example/nine/SecondActivity.java
@@ -3,6 +3,8 @@ package com.example.nine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initListeners();
+    }
+
+    private void initListeners() {
+        TextView back =findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
